7c2065596105efca149bcf3ec6972aa608668b17
Victoria <vicktoria.sin@yandex.ru>
2024-09-08 22:28:08 +0300

    pr fixesfixes

--- src/main/java/org/tbank/Main.java
+++ src/main/java/org/tbank/Main.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.tbank.model.City;
 import org.tbank.util.JsonReader;
 import org.tbank.util.XmlWriter;
 
