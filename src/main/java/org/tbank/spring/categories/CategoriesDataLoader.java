db96616609519d6b8d4640981ce3cd879f9bc8ec
Victoria <vicktoria.sin@yandex.ru>
2024-09-26 01:04:02 +0300

    common data loader

--- src/main/java/org/tbank/spring/categories/CategoriesDataLoader.java
+++ src/main/java/org/tbank/spring/categories/CategoriesDataLoader.java
@@ -1,45 +1,27 @@
 package org.tbank.spring.categories;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 import org.tbank.spring.aop.Timed;
+import org.tbank.spring.common.CommonDataLoader;
 import org.tbank.spring.common.DataSource;
 
 @Component
 public class CategoriesDataLoader {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CategoriesDataLoader.class);
+    private static final String URL = "https://kudago.com/public-api/v1.4/place-categories/";
 
     @Autowired
     private DataSource<Integer, Category> dataSource;
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    @Autowired
+    private CommonDataLoader commonDataLoader;
 
     @Timed
     @EventListener(ApplicationReadyEvent.class)
     public void loadData() {
-        Category[] categories;
-        try {
-            LOGGER.info("Начинаю грузить Categories.");
-            categories = restTemplate.getForEntity("https://kudago.com/public-api/v1.4/place-categories/", Category[].class).getBody();
-        } catch (RestClientException e) {
-            LOGGER.info("Не смогли загрузить Categories.");
-            return;
-        }
-        if (categories == null) {
-            LOGGER.info("Categories нет.");
-            return;
-        }
-        LOGGER.info("Сохраняю Categories ({}) в базу данных.", categories.length);
-        for (Category category : categories) {
-            dataSource.put(category);
-        }
-        LOGGER.info("Сохранил Categories в базу данных.");
+        commonDataLoader.loadData(Category.class, URL, dataSource);
     }
 }
--- src/main/java/org/tbank/spring/locations/LocationsDataLoader.java
+++ src/main/java/org/tbank/spring/locations/LocationsDataLoader.java
@@ -1,45 +1,27 @@
 package org.tbank.spring.locations;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 import org.tbank.spring.aop.Timed;
+import org.tbank.spring.common.CommonDataLoader;
 import org.tbank.spring.common.DataSource;
 
 @Component
 public class LocationsDataLoader {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(LocationsDataLoader.class);
+    private static final String URL = "https://kudago.com/public-api/v1.4/locations/";
 
     @Autowired
     private DataSource<Integer, Location> dataSource;
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    @Autowired
+    private CommonDataLoader commonDataLoader;
 
     @Timed
     @EventListener(ApplicationReadyEvent.class)
     public void loadData() {
-        Location[] locations;
-        try {
-            LOGGER.info("Начинаю грузить Locations.");
-            locations = restTemplate.getForEntity("https://kudago.com/public-api/v1.4/locations/", Location[].class).getBody();
-        } catch (RestClientException e) {
-            LOGGER.info("Не смогли загрузить Locations.");
-            return;
-        }
-        if (locations == null) {
-            LOGGER.info("Locations нет.");
-            return;
-        }
-        LOGGER.info("Сохраняю Locations ({}) в базу данных.", locations.length);
-        for (Location location : locations) {
-            dataSource.put(location);
-        }
-        LOGGER.info("Сохранил Locations в базу данных.");
+        commonDataLoader.loadData(Location.class, URL, dataSource);
     }
 }
